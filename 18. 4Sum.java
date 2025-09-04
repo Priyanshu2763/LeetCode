77370fa3b26eac7d75b02931781a0b57a458fe28
Priyanshu Prasad Gupta <119522534+Priyanshu2763@users.noreply.github.com>
2025-09-04 18:29:09 +0530

    Add files via upload

---
+++ 18. 4Sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>>l=new ArrayList<>();
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i-1]==nums[i]){
+                continue;
+            }
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && nums[j-1]==nums[j]){
+                continue;
+            }
+               int k=j+1;
+               int m=n-1;
+               while(k<m){
+                long sum = (long)nums[i] + (long)nums[j] + (long)nums[k] + (long)nums[m];
+
+                if(sum>target){
+                    m--;
+                }else if(sum<target){
+                    k++;
+                }else{
+                    l.add(Arrays.asList(nums[i],nums[j],nums[k],nums[m]));
+                    k++;m--;
+                    while(k<m && nums[k]==nums[k-1]){ k++; }
+                    while(k<m && nums[m]==nums[m+1]){ m--;; }
+                }
+
+               }
+            }
+        }
+        return l;
+
+        
+    }
+}
